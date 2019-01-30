5535e54188bd90b9e3cbe2f24d4376b97c24c246
eayllon1 <eayllon1@gmail.com>
2019-01-30 14:52:38 -0500

    Changed framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-class App extends Component {
+export default class App extends React.Component {
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('http://localhost:3001/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <View style={styles.container}>
+        <Text>Users</Text>
+        {this.state.users.map(user =>
+          <Text key={user.id}>{user.username}</Text>
+        )}
+      </View>
     );
   }
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
