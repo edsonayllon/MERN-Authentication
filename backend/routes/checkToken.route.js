3cb55ff568f379a6a9a7e3632ab2f7badc98515b
eayllon1 <eayllon1@gmail.com>
2019-02-13 18:54:59 -0500

    updated server

--- backend/routes/checkToken.route.js
+++ backend/routes/checkToken.route.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const config = require('../config')
-const secret = config.TOKEN_SECRET;
 
 router.get('/', function(req, res, next) {
-  jwt.verify(req.token, secret, (err, decoded) => {
-    if(err){
-      res.sendStatus(403);
-    } else {
-      //If token is successfully verified, we can send the autorized data
-      res.sendStatus(200);
-    }
-  })
+  res.sendStatus(200);
 });
 
 module.exports = router;
--- backend/routes/secret.route.js
+++ backend/routes/secret.route.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const config = require('../config')
-const secret = config.TOKEN_SECRET;
 
 router.get('/', function(req, res, next) {
-  jwt.verify(req.token, secret, (err, decoded) => {
-     if(err){
-       //If error send Forbidden (403)
-       console.log('ERROR: Could not connect to the protected route');
-       res.sendStatus(403);
-     } else {
-       //If token is successfully verified, we can send the autorized data
-       res.send('The password is potato');
-       console.log('SUCCESS: Connected to protected route');
-     }
-  })
+   res.send('The password is potato');
+   console.log('SUCCESS: Connected to protected route');
 });
 
 module.exports = router;
